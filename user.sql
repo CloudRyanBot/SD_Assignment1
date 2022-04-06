a771f7e1c978ba32620e13b1b3137abd515c0468
CloudRyanBot <79503931+CloudRyanBot@users.noreply.github.com>
2022-04-06 09:22:14 +0300

    Create user.sql

---
+++ user.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `user` (
+  `userid` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` int NOT NULL,
+  `user` varchar(255) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+INSERT INTO user VALUES(1, 'troll@fnlk', '1234',0,'troll');
+INSERT INTO user VALUES(2, 'admin@admin.admin', 'admin',1,'admin');
+INSERT INTO user VALUES(3, 'use2@user.com', 'acum',0,'user2');
+INSERT INTO user VALUES(4, 'potate@pot.com', 'nu',0,'nice');
+INSERT INTO user VALUES(5, 'tray@fj.ro', 'hunter2',0,'Fighter');
