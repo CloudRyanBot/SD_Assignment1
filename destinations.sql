ae761d005fdacbea29d9b0d70e53dfd6633832f0
CloudRyanBot <79503931+CloudRyanBot@users.noreply.github.com>
2022-04-06 09:50:19 +0300

    Update destinations.sql

--- destinations.sql
+++ destinations.sql
@@ -2,7 +2,7 @@ CREATE TABLE `destination` (
   `destination_id` int NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`destination_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+)
 
 INSERT INTO destination VALUES(1, "Cluj");
 INSERT INTO destination VALUES(2, "Orastie");
